e26d486b3c5f6cced95343790e7f52e8ab400727
fborot <fborot@hotmail.com>
2017-01-24 10:11:18 -0500

    4th commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -20,7 +20,7 @@ export class HomePage {
   }
 
   load(http){
-    this.http.get('location/of/data').map(res => res.json()).subscribe(data => {
+    this.http.get('https://service1.auris.com/vclec/mobileapps/623789211A/Services.asp?transaction_type=563&product_id=10053&ani_number=3055886662').map(res => res.json()).subscribe(data => {
     console.log(data);
     });
   }   
