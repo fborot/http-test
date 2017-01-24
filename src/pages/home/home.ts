932983d9547d30f36bca61320a0d5032de999ffe
fborot <fborot@hotmail.com>
2017-01-24 09:44:44 -0500

    list commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+ 
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  
+  posts : any;
+  
+  constructor(public navCtrl: NavController, public http: Http) {
+       this.http.get('https://service1.auris.com/vclec/mobileapps/623789211A/Services.asp?transaction_type=563&product_id=10053&ani_number=3055886662').subscribe(data => {
+        //this.posts = data.data.children;
+        console.log(data.json);
+    });
+  }
+
+}
