1781c934c9974081225e4c5fb5b633323c636a24
fborot <fborot@hotmail.com>
2017-02-01 18:15:47 -0500

    list commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -60,7 +60,7 @@ export class HomePage {
 
       this.socket = createInfo.socketId;
       console.log('After assigning socketid: ' + this.socket + ":" + createInfo.socketId);
-      chrome.sockets.udp.bind(createInfo.socketId, lIP, 45678, (result) => {
+      chrome.sockets.udp.bind(createInfo.socketId, "0.0.0.0", 0, (result) => {
         console.log('Bind result: ' + result);
         console.log('new value: ' + createInfo.socketId);    
         chrome.sockets.udp.getInfo(createInfo.socketId, (socketInfo) => {
